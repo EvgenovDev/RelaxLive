61094e050c15d535166eef5e68195e5dd3882437
Roman Evgenov <Roman Evgenov>
2021-11-17 21:52:19 +0400

    Правки

--- crm-backend/src/bundle.js
+++ crm-backend/src/bundle.js
@@ -1,3 +1,3 @@
 import togglePhoneNumber from "./modules/headerPhone";
 
-togglePhoneNumber("header-contacts__phone-number", "header-contacts__arrow");
+togglePhoneNumber("eader-contacts__phone-number", "header-contacts__arrow");
--- crm-backend/src/modules/headerPhone.js
+++ crm-backend/src/modules/headerPhone.js
@@ -2,9 +2,17 @@ const togglePhoneNumber = (phoneClass, arrowClass) => {
   const phones = document.querySelectorAll(`.${phoneClass}`);
   const arrow = document.querySelector(`.${arrowClass}>img`);
 
-  arrow.addEventListener("click", (e) => {
-    phones[1].classList.toggle("header-contacts__phone-number-accord--active");
-  });
+  try {
+    if (phones.length && arrow) {
+      arrow.addEventListener("click", (e) => {
+        phones[1].classList.toggle("header-contacts__phone-number-accord--active");
+      });
+    } else {
+      throw Error("Классы телефона или стрелки переданы не верно");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export default togglePhoneNumber;
