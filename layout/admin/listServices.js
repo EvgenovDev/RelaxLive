891df5e9b610ef69a97db43e2e2b4276220fea3f
Roman Evgenov <Roman Evgenov>
2021-11-25 21:57:13 +0400

    Добавление новой услуги

--- layout/admin/listServices.js
+++ layout/admin/listServices.js
@@ -1,9 +1,18 @@
 const select = document.querySelector("select");
 const tableBody = document.querySelector("#tbody");
 const tr = document.querySelector(".table__row");
+const button = document.querySelector(".btn-addItem");
+const popup = document.querySelector(".modal__overlay");
 
 function getData(url) {
   return fetch(url);
+}
+
+function setNewServices(url, dataObject, method) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(dataObject)
+  });
 }
 
 function clear(elem) {
@@ -11,6 +20,7 @@ function clear(elem) {
 }
 
 function renderSelectOption(array) {
+  clear(select);
   let newOption = new Option("Все услуги", "Все услуги");
   select.append(newOption);
   array.forEach((elem) => {
@@ -20,6 +30,7 @@ function renderSelectOption(array) {
 }
 
 function renderTable(array) {
+  clear(tableBody);
   array.forEach((elem) => {
     let newTr = tr.cloneNode(true);
     newTr.querySelector(".table__id").textContent = elem.id;
@@ -35,7 +46,6 @@ if (!document.cookie || document.cookie !== "admin=true") {
   window.location.href = `${document.location.href}/../index.html`;
 } else {
   clear(select);
-  clear(tableBody);
   getData("http://localhost:3000/api/items")
     .then((response) => {
       return response.json();
@@ -62,10 +72,63 @@ if (!document.cookie || document.cookie !== "admin=true") {
         return response.json();
       })
       .then((data) => {
-        let arr = [];
-        arr = data.filter(elem => elem.type == valueOption);
-        clear(tableBody);
-        renderTable(arr);
+        if (valueOption == "Все услуги") {
+          renderTable(data);
+        } else {
+          let arr = [];
+          arr = data.filter(elem => elem.type == valueOption);
+          renderTable(arr);
+        }
       });
+  });
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("show-modal");
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target.closest(".icon__close") || !e.target.closest(".modal") || e.target.closest(".cancel-button")) {
+      e.preventDefault();
+      modal.classList.remove("show-modal");
+    } else if (e.target.closest(".button-ui_firm")) {
+      e.preventDefault();
+      const inputs = modal.querySelectorAll("input");
+      const data = {
+        type: inputs[0].value,
+        name: inputs[1].value,
+        units: inputs[2].value,
+        cost: +inputs[3].value
+      };
+      setNewServices("http://localhost:3000/api/items", data, "POST");
+      getData("http://localhost:3000/api/items")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let arr = [];
+          if (data[0]) {
+            let type = data[0].name;
+            arr = data.filter((elem) => {
+              if (elem.type != type) {
+                type = elem.type;
+                return elem.type;
+              }
+            });
+            renderSelectOption(arr);
+          }
+          select.selectedIndex = select.options.length - 1;
+          const valueOption = select[select.selectedIndex].value;
+
+          let arrForTable = [];
+          arrForTable = data.filter(elem => elem.type == valueOption);
+          clear(tableBody);
+          renderTable(arrForTable);
+          inputs.forEach((input) => {
+            input.value = "";
+          });
+          modal.classList.remove("show-modal");
+        });
+    }
   });
 }
